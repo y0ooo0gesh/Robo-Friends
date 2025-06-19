459730f128985cce168e28d06680f2efdad5bd3c
y0ooo0gesh <kvsyogesh703@gmail.com>
2025-06-19 17:20:16 +0530

    Initial Commit

---
+++ src/cards.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Card = () =>{
+    return(
+        <div>
+            <img src=''/>
+            <div>
+                <h2>Monotone</h2>
+                <p>mono.tone@gmail.com</p>
+            </div>
+        </div>
+    )
+}
