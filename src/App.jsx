f33301f106b0fe545db4242a53d56128a6dfe85b
y0ooo0gesh <kvsyogesh703@gmail.com>
2025-06-22 00:23:54 +0530

    Can now search

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./cardList";
 import { robots } from './robot.js';
 import SearchBox from "./searchBox.jsx";
 
-const App = () => {
-    return(
-        <div className="tc ">
-            
-            <h1>Robo-Friends Here</h1>
-            <SearchBox/>
-            <CardList robotic={robots}/> 
-        </div>
-    )
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            robotipo: robots,
+            searchfield: ""
+        }
+    }
+
+    finder = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
+    render(){
+        const filterRobo = this.state.robotipo.filter(robot =>{
+            return robot.realname.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+        return(
+            <div className="tc ">
+                <h1>Robo-Friends Here</h1>
+                <SearchBox searcher={this.finder}/>
+                <CardList robotic={filterRobo}/> 
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/searchBox.jsx
+++ src/searchBox.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-const SearchBox = () => {
+const SearchBox = ( {searcher} ) => {
     return(
         <div className=" pa2 ">
         <input 
             className="pa3 tc w-20 br-pill grow  bw1 ba b--blue bg-light-green"
             type="search" 
             placeholder="Search Your ROBO-Friends"
+            onChange={searcher}
         />
         </div>
     )}
