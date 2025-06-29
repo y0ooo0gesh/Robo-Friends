8a34bd03238a7290d6a96ef1d4993c8f0228e2f4
y0ooo0gesh <kvsyogesh703@gmail.com>
2025-06-29 18:50:42 +0530

    Minor Fix

--- src/components/searchBox.jsx
+++ src/components/searchBox.jsx
@@ -6,7 +6,7 @@ const SearchBox = ( {searcher} ) => {
             <input 
                 className="pa3 ma4 ph4 tc w-auto br-pill bw1 ba b--blue bg-light-green"
                 type="search" 
-                placeholder="Search Your ROBO-Friends"
+                placeholder="Search Your new Friend"
                 onChange={searcher}
             />
         </div>
