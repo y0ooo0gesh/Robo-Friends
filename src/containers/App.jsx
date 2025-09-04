082aca168e83b065590cc370a581a3ae74845236
y0ooo0gesh <kvsyogesh703@gmail.com>
2025-09-04 18:52:35 +0530

    Turned class component into Hooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,50 +1,43 @@
-import React, { Component } from "react";
+import React, { useEffect, useState} from "react";
 import CardList from "../components/cardList.jsx";
 import SearchBox from "../components/searchBox.jsx";
 import Scroll from "../components/scroll.jsx";
 import ErrorBoundary from "../components/ErrorBoundary.jsx";
 
-class App extends Component {
-    constructor(){
-        super()
-        this.state = {
-            robotipo: [],
-            searchfield: ""
-        }
-    }
+export default function App() {
+    const [robots, setrobots] = useState([]);
+    const [searchfield, setsearchfield] = useState("");
 
-componentDidMount(){
+    useEffect(()=>{
     fetch('https://jsonplaceholder.typicode.com/users')
         .then(Response=> Response.json())
-        .then(users => this.setState({ robotipo: users }))
-}    
+        .then(users => setrobots(users))
+    },[])
+    //dependdencies in useEffect is to tell run when  this dependencies changes and [] means that run only ones when app renders 
+    // note do not forget dependencies or it will keep going in infinite loop
 
-    finder = (event) => {
-        this.setState({ searchfield: event.target.value })
+    const finder = (event) => {
+        setsearchfield(event.target.value);
     }
-    render(){
-        const { robotipo, searchfield }= this.state;
-        const filterRobo = robotipo.filter(robot =>{
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-        })
-        return !robotipo.length ?
-        <h1 className="ma0 pa2">Loading Please Wait</h1>:
-        (<>
-            <div className="tc flex flex-wrap flex-column items-center justify-center">
-                <div className="f1">
-                    <h1>Robo-Friends Here</h1>
-                </div> 
-                <SearchBox searcher={this.finder}/>
+
+    const filteredRobo = robots.filter(robot =>{
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    })
+    return !robots.length ?
+    <h1 className="ma0 pa2">Loading Please Wait</h1>:
+    (<>
+        <div className="tc flex flex-wrap flex-column items-center justify-center">
+            <div className="f1">
+                <h1>Robo-Friends Here</h1>
+            </div> 
+            <SearchBox searcher={finder}/>
+        </div>
+        <Scroll>
+            <div className="flex flex-wrap justify-center">
+                <ErrorBoundary>
+                    <CardList robotic={filteredRobo}/> 
+                </ErrorBoundary>
             </div>
-            <Scroll>
-                <div className="flex flex-wrap justify-center">
-                    <ErrorBoundary>
-                        <CardList robotic={filterRobo}/> 
-                    </ErrorBoundary>
-                </div>
-            </Scroll>
-        </>)
-    }
+        </Scroll>
+    </>)
 }
-
-export default App;
